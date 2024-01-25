4f5fa578639d73939346a1309d41b7d606dbb204
Maria Emilia Torino <emilia.torino@canonical.com>
2024-01-25 10:25:08 -0300

    Moving exit logic to the correct location
    Signed-off-by: Maria Emilia Torino <emilia.torino@canonical.com>

--- dsc2jira.py
+++ dsc2jira.py
@@ -204,9 +204,9 @@ if __name__ == "__main__":
         sys.exit(-1)
     try:
         start_date = parse_date_duration(args.duration)
-        sys.exit(-1)
     except ValueError as e:
         logging.error(f"Unable to resolve start_date: {e}")
+        sys.exit(-1)
     logging.debug(f"{start_date}")
     logging.debug("Using {} time period".format(args.duration))
 
